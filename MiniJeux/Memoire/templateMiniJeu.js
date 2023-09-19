e248d1593fee1924784046e173b00f3f34db9f73
jordan8022 <jordan.moritz@epsi.fr>
2023-09-19 16:00:29 +0200

    chiffre aléatoire

--- MiniJeux/Memoire/templateMiniJeu.js
+++ MiniJeux/Memoire/templateMiniJeu.js
@@ -1,14 +1,5 @@
 import MiniJeu from '../MiniJeu.js'
 
-class Case {
-    constructor(y,x){
-        this.x=x
-        this.y=y
-    }
-
-    
-}
-
 
 export default class MemoireJeu extends MiniJeu {
     /*
@@ -18,6 +9,9 @@ export default class MemoireJeu extends MiniJeu {
         }
     Et le jeu s'arrêtera.
     */
+
+
+
 
     constructor(canvas, ctx, tailleX, tailleY) {
         super(canvas, ctx, tailleX, tailleY);
@@ -40,15 +34,66 @@ export default class MemoireJeu extends MiniJeu {
         } else {
             alert("Vous avez annulé la saisie.");
         }
+
+
+        this.valeur = [];
+
+        if (this.nombrepaires == 4 || this.nombrepaires == 6 || this.nombrepaires == 8) {
+
+            for (let i = 0; i < this.nombrepaires / 2; i++) {
+                let temp = [];
+
+                for (let j = 0; j < 4; j++) {
+                    temp.push(0);
+                }
+
+                this.valeur.push(temp);
+            }
+
+            let paires = [];
+            for (let i = 1; i <= this.nombrepaires; i++) paires.push(i);
+
+
+            for (let i = 0; i < paires.length; i++) {
+
+                for (let j = 0; j < 2; j++) {
+
+                    let pose = false;
+                    while (!pose) {
+                        let x = Math.round(Math.random() * (this.nombrepaires/2-1));
+                        let y = Math.round(Math.random() * 3);
+
+                        if (this.valeur[x][y] == 0) {
+                            pose = true;
+                            this.valeur[x][y] = paires[i];
+                        }
+                    }
+                }
+            }
+            console.log(this.valeur)
+
+
+        }
         return
         //Trucs à faire en début de programme
     }
 
 
     Update() {// Dessiner les 12 cartes séparées par des espaces
+        var aleatoire = 0;
+
+/*
+
+        if (this.nombrepaires == 6) {
+            valeur = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+        }
+        if (this.nombrepaires == 8) {
+            valeur = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+        }
+
+*/
         for (let row = 0; row < this.nombrepaires / 2; row++) {
             for (let col = 0; col < 4; col++) {
-
                 var y = col * (this.squareSize + 20); // Espacement horizontal
                 var x = row * (this.squareSize + 20); // Espacement vertical
 
