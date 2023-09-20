f72eedd30a7a3a40a9ff6991e3fd2e3554dc370f
jordan8022 <jordan.moritz@epsi.fr>
2023-09-20 16:41:39 +0200

    cliquable et plus qu'a montrer les chiffres au debut

--- MiniJeux/Memoire/templateMiniJeu.js
+++ MiniJeux/Memoire/templateMiniJeu.js
@@ -10,7 +10,11 @@ export default class MemoireJeu extends MiniJeu {
     Et le jeu s'arrêtera.
     */
 
+    dessinerCarre(x, y, largeur, hauteur, numero) {
+        this.ctx.fillStyle = 'white'; // Couleur du carré
+        this.ctx.fillRect(x, y, largeur, hauteur);
 
+    }
 
 
     constructor(canvas, ctx, tailleX, tailleY) {
@@ -38,7 +42,6 @@ export default class MemoireJeu extends MiniJeu {
             alert("Vous avez annulé la saisie.");
         }
 
-
         this.valeur = [];
 
         if (this.nombrepaires == 4 || this.nombrepaires == 6 || this.nombrepaires == 8) {
@@ -55,7 +58,6 @@ export default class MemoireJeu extends MiniJeu {
 
             let paires = [];
             for (let i = 1; i <= this.nombrepaires; i++) paires.push(i);
-
 
             for (let i = 0; i < paires.length; i++) {
 
@@ -75,8 +77,97 @@ export default class MemoireJeu extends MiniJeu {
             }
             console.log(this.valeur)
 
-
         }
+
+        this.carres = [];
+
+        if (reponse == 4) {
+            this.carres = [
+                { x: 75, y: 75, largeur: 75, hauteur: 75, numero: -1 },
+                { x: 75, y: 150, largeur: 75, hauteur: 75, numero: -1 },
+                { x: 75, y: 225, largeur: 75, hauteur: 75, numero: -1 },
+                { x: 75, y: 300, largeur: 75, hauteur: 75, numero: -1 },
+                { x: 150, y: 75, largeur: 75, hauteur: 75, numero: -1 },
+                { x: 150, y: 150, largeur: 75, hauteur: 75, numero: -1 },
+                { x: 150, y: 225, largeur: 75, hauteur: 75, numero: -1 },
+                { x: 150, y: 300, largeur: 75, hauteur: 75, numero: -1 },
+
+            ];
+        }
+
+        if (reponse == 6) {
+            this.carres = [
+                { x: 75, y: 75, largeur: 75, hauteur: 75 },
+                { x: 75, y: 150, largeur: 75, hauteur: 75 },
+                { x: 75, y: 225, largeur: 75, hauteur: 75 },
+                { x: 75, y: 300, largeur: 75, hauteur: 75 },
+                { x: 150, y: 75, largeur: 75, hauteur: 75 },
+                { x: 150, y: 150, largeur: 75, hauteur: 75 },
+                { x: 150, y: 225, largeur: 75, hauteur: 75 },
+                { x: 150, y: 300, largeur: 75, hauteur: 75 },
+                { x: 225, y: 75, largeur: 75, hauteur: 75 },
+                { x: 225, y: 150, largeur: 75, hauteur: 75 },
+                { x: 225, y: 225, largeur: 75, hauteur: 75 },
+                { x: 225, y: 300, largeur: 75, hauteur: 75 },
+
+            ];
+        }
+
+        if (reponse == 8) {
+            this.carres = [
+                { x: 75, y: 75, largeur: 75, hauteur: 75 },
+                { x: 75, y: 150, largeur: 75, hauteur: 75 },
+                { x: 75, y: 225, largeur: 75, hauteur: 75 },
+                { x: 75, y: 300, largeur: 75, hauteur: 75 },
+                { x: 150, y: 75, largeur: 75, hauteur: 75 },
+                { x: 150, y: 150, largeur: 75, hauteur: 75 },
+                { x: 150, y: 225, largeur: 75, hauteur: 75 },
+                { x: 150, y: 300, largeur: 75, hauteur: 75 },
+                { x: 225, y: 75, largeur: 75, hauteur: 75 },
+                { x: 225, y: 150, largeur: 75, hauteur: 75 },
+                { x: 225, y: 225, largeur: 75, hauteur: 75 },
+                { x: 225, y: 300, largeur: 75, hauteur: 75 },
+                { x: 300, y: 75, largeur: 75, hauteur: 75 },
+                { x: 300, y: 150, largeur: 75, hauteur: 75 },
+                { x: 300, y: 225, largeur: 75, hauteur: 75 },
+                { x: 300, y: 300, largeur: 75, hauteur: 75 },
+
+            ];
+        }
+
+        function onclick(event) {
+            const rect = canvas.getBoundingClientRect(); // Obtient la position du canvas sur la page
+            const xClic = event.clientX - rect.left; // Position horizontale du clic
+            const yClic = event.clientY - rect.top; // Position verticale du clic
+
+
+
+            // Vérifiez si le clic est à l'intérieur de l'un des carrés
+            for (const carre of this.carres) {
+                if (
+                    xClic >= carre.x &&
+                    xClic <= carre.x + carre.largeur &&
+                    yClic >= carre.y &&
+                    yClic <= carre.y + carre.hauteur
+                ) {
+
+                    alert('Bravo !');
+                    return; // Sortez de la boucle dès qu'un carré est cliqué
+                }
+            }
+        }
+        console.log(this.valeur);
+
+        for (let i = 0; i < this.carres.length; i++) {
+            let carre = this.carres[i];
+
+            this.carres[i]["val"] = this.valeur[carre.x / 75 - 1][carre.y / 75 - 1];
+        };
+
+        console.log(this.carres);
+
+        canvas.addEventListener('click', onclick.bind(this));
+
         return
         //Trucs à faire en début de programme
     }
@@ -90,30 +181,39 @@ export default class MemoireJeu extends MiniJeu {
             this.cartesAffichees = false;
         }
 
+        this.carres.forEach(carre => {
+            this.dessinerCarre(carre.x, carre.y, carre.largeur, carre.hauteur, carre.numero);
+            if (this.cartesAffichees) {
 
-        for (let row = 0; row < this.nombrepaires / 2; row++) {
+                this.ctx.fillStyle = 'black'
+                this.ctx.fillText(this.valeur[carre.x / 75 - 1][carre.y / 75 - 1], carre.x + 20, carre.y + 60);
+            }
+        });
+
+
+        /*for (let row = 0; row < this.nombrepaires / 2; row++) {
             for (let col = 0; col < 4; col++) {
                 var y = col * (this.squareSize + 20); // Espacement horizontal
                 var x = row * (this.squareSize + 20); // Espacement vertical
 
                 this.ctx.fillStyle = "white";
                 this.ctx.fillRect(x, y, this.cardSize, this.cardSize);
-              /*  const x = event.client - rect.left;
-                const y = event.client - rect.top;*/
+                /*  const x = event.client - rect.left;
+                  const y = event.client - rect.top;*/
 
 
-                // Vous pouvez personnaliser chaque carte ici
-                // Par exemple, vous pouvez ajouter du texte ou des images
+        // Vous pouvez personnaliser chaque carte ici
+        // Par exemple, vous pouvez ajouter du texte ou des images
 
-                this.ctx.strokeStyle = "black";
-                this.ctx.strokeRect(x, y, this.cardSize, this.cardSize);
-                if (this.cartesAffichees) {
+        /* this.ctx.strokeStyle = "black";
+         this.ctx.strokeRect(x, y, this.cardSize, this.cardSize);
+         if (this.cartesAffichees) {
 
-                    this.ctx.fillStyle = "black";
+             this.ctx.fillStyle = "black";
 
-                    this.ctx.fillText(this.valeur[row][col], x + 20, y + 50);
-                }
-            }
-        }
+             this.ctx.fillText(this.valeur[row][col], x + 20, y + 50);
+         }
+     }
+ }*/
     }
 }
